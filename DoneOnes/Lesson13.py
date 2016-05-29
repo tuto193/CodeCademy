68e4b6dfe3d67ec688b29ca39ef57d80449e1576
Carlos A. Parra <carlos.a.parra.f@gmail.com>
2016-05-29 11:22:02 +0200

    Lesson 14

---
+++ DoneOnes/Lesson13.py
@@ -0,0 +1,15 @@
+class Employee( object ):
+    """Models real-life employees!"""
+    def __init__( self, employee_name ):
+        self.employee_name = employee_name
+
+    def calculate_wage( self, hours ):
+        self.hours = hours
+        return hours * 20.00
+
+#add my own code underneath
+class PartTimeEmployee( Employee ):
+    """Only works part time"""
+    def calculate_wage( self, hours ):
+        self.hours = hours
+        return hours * 12.00
